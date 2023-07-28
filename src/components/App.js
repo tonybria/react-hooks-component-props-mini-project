8a1d1e1f309fffb5f803a59ec27882a38a2ba150
tony-brian <btoni3823@gmail.com>
2023-07-28 09:44:15 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,36 @@ import blogData from "../data/blog";
 
 console.log(blogData);
 
-function App() {
+function App() {const [tasks, setTasks] = useState(TASKS);
+	const [category, setCategory] = useState("All");
+
+	function handleAddTask(newTask) {
+		setTasks([...tasks, newTask]);
+	}
+
+	function handleDeleteTask(deletedTaskText) {
+		setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+	}
+
+	const visibleTasks = tasks.filter(
+		(task) => category === "All" || task.category === category
+	);
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <h2>My tasks</h2>
+			<CategoryFilter
+				categories={CATEGORIES}
+				selectedCategory={category}
+				onSelectCategory={setCategory}
+			/>
+			<div className="tasks">
+				<h5>Tasks</h5>
+				<NewTaskForm
+					categories={CATEGORIES.filter((cat) => cat !== "All")}
+					onTaskFormSubmit={handleAddTask}
+				/>
+				<TaskList onDeleteTask={handleDeleteTask} tasks={visibleTasks} />
+			</div>
     </div>
   );
 }
